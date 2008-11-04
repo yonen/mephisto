b8b5d35785c2ef2c960ca00c7eee0270b3e9522b
yonen <jessy.ouellette@gmail.com>
2008-11-04 07:05:15 -0700

    Fixed file uploading issue when editing article

--- public/javascripts/mephisto/application.js
+++ public/javascripts/mephisto/application.js
@@ -206,9 +206,9 @@ TinyTab.prototype = {
 Asset = {
   upload: function(form) {
     form = $(form);
-    article_id   = location.href.match(/\/(edit|upload)\/([0-9]+)/);
+    article_id   = location.href.match(/\/([0-9]+)\/(edit|upload)/);
     form.action  = Mephisto.root + "/admin/articles/upload"
-    if(article_id) form.action += "/" + article_id[2]
+    if(article_id) form.action += "/" + article_id[1]
     form.submit();
   },
   
